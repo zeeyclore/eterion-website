465a4bc191b5e39f4cfa9e171d258cf7218c02f5
syifanalfiah <syifakenari12@gmail.com>
2025-07-12 14:56:23 +0700

    coba dulu

--- src/app/members/(container)/lists/nrp052.tsx
+++ src/app/members/(container)/lists/nrp052.tsx
@@ -6,11 +6,11 @@ import Image from "next/image";
 import { useState } from "react";
 
 const data = {
-  name: "John Doe",
-  nrp: "5027241000",
-  image: "000.jpg",
-  funfact: "kalo makan pake nasi, nasinya harus dari beras",
-  hobby: "makan nasi dari beras",
+  name: "Salsa Bil Ulla",
+  nrp: "5027241052",
+  image: "052.jpg",
+  funfact: "Pernah fomo jadi YouTuber Gaming pas SD",
+  hobby: "Main musik, Main game, Nonton, Dance",
   origin: "Surabaya",
 };
 
